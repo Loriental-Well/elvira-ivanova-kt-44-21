2c96fa307eb4cfc817f5578d5e37d957ea227466
ivanova-elvira-kt-44-21 <elvira.ivanova.2003@mail.ru>
2024-10-25 17:14:38 +0300

    6 laba

---
+++ ElviraIvanovaKt-44-21.Tests/GroupTests.cs
@@ -0,0 +1,23 @@
+﻿using ElviraIvanovaKt_44_21.Models;
+
+namespace ElviraIvanovaKt_44_21.Tests
+{
+    public class GroupTests
+    {
+        [Fact]
+        public void IsValidGroupName_KT4421_True()
+        {
+            //arrange
+            var testGroup = new Group
+            {
+                GroupName = "KT-31-20"
+            };
+
+            //act
+            var result = testGroup.IsValidGroupName();
+
+            //assert
+            Assert.True(result);
+        }
+    }
+}
